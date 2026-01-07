2c59d328b443981e1ff7e84cacc7a46f0149acf6
Gatorolo-rolo <>
2026-01-07 03:34:32 -0300

    corrigiendo cors

--- src/main/java/com/back_hack/security/config/SecurityConfig.java
+++ src/main/java/com/back_hack/security/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedOrigins(Arrays.asList("https://hackathon-alura.web.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
