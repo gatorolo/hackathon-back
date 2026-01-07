9bd105abf6c88f93f7b5b624f226118b6b0e4030
Gatorolo-rolo <>
2026-01-07 06:51:27 -0300

    corrigiendi 3

--- src/main/java/com/back_hack/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/back_hack/security/jwt/JwtAuthenticationFilter.java
@@ -14,46 +14,19 @@ import java.io.IOException;
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    /*@Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
-
-        final String token = getTokenfromRequest(request);
-
-        if (token == null) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-        filterChain.doFilter(request, response);
-
-    }*/
-
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-        String path = request.getServletPath();
-
-        // LOG de depuración (lo verás en la consola de Koyeb)
-        System.out.println("Filtro ejecutándose para la ruta: " + path);
-
-        // 1. SI ES AUTH, PASAR DE LARGO SIN PREGUNTAR
-        if (path.startsWith("/auth/")) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-
         final String token = getTokenfromRequest(request);
 
-        // 2. Si no hay token en otras rutas, sigue normal (Spring rebotará si es necesario)
         if (token == null) {
             filterChain.doFilter(request, response);
             return;
         }
-
         filterChain.doFilter(request, response);
-    }
 
+    }
 
         private String getTokenfromRequest (HttpServletRequest request){
 
